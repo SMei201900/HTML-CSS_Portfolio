977eb8d16e34a3fb894f6385654c29b154141269
Sally M <smei201900@gmail.com>
2025-08-03 19:18:32 -0400

    created empty functions

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ let darkmode = localStorage.getItem("darkmode");
 const themeSwitch = document.getElementById("theme-switch")
 /*"let" and "const" both creates variables but "let" allows the stored value to be changed later*/
 
+const enableDarkMode = () => {
+
+}
+
+
+const disableDarkMode = () => {
+
+}
+
+
+
+
+
+
 themeSwitch.addEventListener("click", () => {
     darkmode !== "active" ? enableDarkMode() : disableDarkMode()
 }) 
